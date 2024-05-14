09c7691257e2a4dea328901b8548ddd3390a40a5
Chris Tang <1073412@hr.nl>
2024-05-14 16:07:08 +0200

    push part 1 of my endcase

--- src/js/game.js
+++ src/js/game.js
@@ -1,6 +1,7 @@
 import '../css/style.css'
 import { Actor, Engine, Vector, DisplayMode } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
+import { Yoshi } from './yoshi.js'
 
 export class Game extends Engine {
 
@@ -16,11 +17,23 @@ export class Game extends Engine {
 
     startGame() {
         console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(400, 300)
-        fish.vel = new Vector(-10,0)
-        this.add(fish)
+        // const car = new Actor()
+        // car.graphics.use(Resources.Car.toSprite())
+        // car.pos = new Vector(200, 300)
+        // car.vel = new Vector(50,0)
+        // this.add(car)
+
+        // const fish = new Actor()
+        // fish.graphics.use(Resources.Fish.toSprite())
+        // fish.pos = new Vector(1000, 300)
+        // fish.vel = new Vector(-50,0)
+        // this.add(fish)
+
+        const yoshi = new Yoshi()
+        yoshi.graphics.use(Resources.Yoshi.toSprite())
+        yoshi.pos = new Vector(1000, 300)
+        // yoshi.vel = new Vector(-50,0)
+        this.add(yoshi)
     }
 }
 
--- src/js/resources.js
+++ src/js/resources.js
@@ -1,16 +1,22 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
+import { Fish } from './fish'
+import { Yoshi } from './yoshi'
 
 // voeg hier jouw eigen resources toe
 const Resources = {
-    Fish: new ImageSource('images/fish.png')
+    Car: new ImageSource('images/car.png'),
+    Fish: new ImageSource('images/fish.png'),
+    Yoshi: new ImageSource('images/yoshi.png'),
 }
 
 
 
 
-const ResourceLoader = new Loader()
-for (let res of Object.values(Resources)) {
-    ResourceLoader.addResource(res)
-}
+
+const ResourceLoader = new Loader([
+    Resources.Car,
+    Resources.Fish,
+    Resources.Yoshi
+])
 
 export { Resources, ResourceLoader }
